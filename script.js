c2380afa1ce00e2cd1bc2bddbec59933117235c5
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 10:40:40 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add your JavaScript code here
+document.addEventListener("DOMContentLoaded", function() {
+    motion.animate({
+        el: document.querySelector('#about'),
+        opacity: [0, 1],
+        duration: 1.5,
+        delay: 0.5
+    });
+
+    // Add more animations for other sections
+});
