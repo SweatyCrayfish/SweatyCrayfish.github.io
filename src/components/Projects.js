c518b9fafabd2364c9b5b7f174f8feab73655eae
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 12:49:18 -0500

    Delete src directory

--- src/components/Projects.js
+++
@@ -1,9 +0,0 @@
-//   src/components/Projects.js
-import React from 'react';
-
-const Projects = () => (
-    <div>
-        <h2>Projects</h2>
-        <p>This is the Projects section.</p>
-    </div>
-);
