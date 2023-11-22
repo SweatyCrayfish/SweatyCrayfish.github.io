6f97a4f0ff83de4cd5836cb23192802b1e9c9801
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 12:06:24 -0500

    Create Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+//   src/components/Projects.js
+import React from 'react';
+
+const Projects = () => (
+    <div>
+        <h2>Projects</h2>
+        <p>This is the Projects section.</p>
+    </div>
+);
