bde2f73d8ffbe71c74f9185e1e5b7be4e0890c37
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 12:06:58 -0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+//    src/components/Header.js
+import React from 'react';
+
+const Header = () => (
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+            </ul>
+        </nav>
+    </header>
+);
