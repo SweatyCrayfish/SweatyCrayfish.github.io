b40c80cd3421739dba034e8fbbccdc77cc9b5a25
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 12:07:29 -0500

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+//  src/components/About.js
+import React from 'react';
+
+const About = () => (
+    <div>
+        <h2>About Me</h2>
+        <p>This is the About section.</p>
+    </div>
+);
+
+export default About;
+""",
+    "src/components/Footer.js": """
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        <p>Footer content goes here.</p>
+    </footer>
+);
