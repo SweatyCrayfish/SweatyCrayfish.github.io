9783e37f9a122f9f35b51c9c6d19e60f3a8ec0d7
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 11:34:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const skills = document.querySelectorAll('.skill');
-    skills.forEach(skill => skill.classList.add('fade-in'));
+    skills.forEach((skill, index) => {
+        setTimeout(() => {
+            skill.style.opacity = 1;
+            skill.style.transform = "translateY(0)";
+        }, 100 * index); // Staggered animation
+    });
 });
