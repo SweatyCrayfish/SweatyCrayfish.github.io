2ba9aab403e768686c8d3db28576cd0c36d2cbc8
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 10:32:10 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,13 @@ import { motion } from 'framer-motion';
 const About = () => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       className="about-section"
     >
       <h1>About Me</h1>
-      <p>Hi, I'm Viktor. I specialize in Artificial Intelligence and my main skills are Python, JavaScript, SQL, AWS, HTML, CSS, Computer Vision, LLM, and Generative AI. I have worked in AI Engineering and Data Engineering at Sampled and Kenvue.</p>
+      <p>I specialize in Python, JavaScript, SQL, AWS, HTML, CSS, Computer Vision, LLM, and Generative AI. My experience includes roles in AI Engineering and Data Engineering at Sampled and Kenvue.</p>
       {/* Add more content about yourself here */}
     </motion.div>
   );
