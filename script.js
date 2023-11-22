84a72e199041e7fdb3c491b0198ae6fd55901632
Viktor Veselov <49697696+SweatyCrayfish@users.noreply.github.com>
2023-11-22 12:02:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const skills = document.querySelectorAll('.skill');
-    skills.forEach((skill, index) => {
-        setTimeout(() => {
-            skill.style.opacity = 1;
-            skill.style.transform = 'translateY(0)';
-        }, 100 * index);
-    });
-});
